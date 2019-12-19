30279980c8e90b552b1492a52e00b610922da779
David <davidciungan@yahoo.com>
2019-12-19 21:00:43 +0200

    last

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        byte numar=11;
-        int coloana=1;
+        int numar;
+
+        numar= 11;
+
+        short coloana;
+
+        coloana=1;
+        System.out.println(numar);
+        System.out.println(coloana);
+        float x=3f;
+
+        float sum = numar + coloana;
+        String calcul ="eu am"+ numar +x;
 
 
-        System.out.println("Luat' cu pita!");
 
 
 
