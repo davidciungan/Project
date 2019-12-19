11f1cc1ed2f9d5ecef393942d073a05bdc09eaf3
David <davidciungan@yahoo.com>
2019-12-19 20:36:55 +0200

    look

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        byte numar=11;
+        int coloana=1;
+
+
+
+
+
+
+
+
+    }
+}
