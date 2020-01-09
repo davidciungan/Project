74592ab27c64a056d6b15d8d906968bd2bce6c78
David <davidciungan@yahoo.com>
2020-01-09 20:40:33 +0200

    lab2

---
+++ src/animalrescue/AppMain.java
@@ -0,0 +1,4 @@
+package animalrescue;
+
+public class AppMain {
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,21 +8,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        int numar;
 
-        numar= 11;
 
-        short coloana;
-
-        coloana=1;
-        System.out.println(numar);
-        System.out.println(coloana);
-        float x=3f;
-
-        float sum = numar + coloana;
-        String calcul ="eu am"+ numar +x;
-short maine
-        maine=13
 
 
 
