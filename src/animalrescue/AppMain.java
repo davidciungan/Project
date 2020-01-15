09700e8a942d10b8653547d84a8441203279fe53
David <davidciungan@yahoo.com>
2020-01-15 22:02:34 +0200

    lab2

--- src/animalrescue/AppMain.java
+++ src/animalrescue/AppMain.java
@@ -1,5 +1,7 @@
 package animalrescue;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class AppMain {
 
     public static void main(String[] args) {
@@ -11,6 +13,24 @@ public class AppMain {
         System.out.println(rex.name);
 Dog brunno = new Dog();
         System.out.println(brunno.name);
+
+        //tema
+
+        Animal cow= new Animal();
+
+        System.out.println(cow.Name="maria");
+        System.out.println(cow.HealthLevel=10);
+
+Adopter human = new Adopter();
+        System.out.println(human.Name="Ana");
+        System.out.println(human.money=14);
+
+        Vet medic = new Vet();
+        System.out.println(medic.Name="Gigel");
+        System.out.println(medic.Speciality="ce vrea el");
     }
+
+
+
 
 }
--- src/animalrescue/Dog.java
+++ src/animalrescue/Dog.java
@@ -18,5 +18,38 @@ public class Dog {
             }
 
             public void eat(){
-                System.out.println("Eating whatever"); }
+                System.out.println("Eating whatever"); }}
+
+
+                class Animal {
+
+     String Name="Cat";
+     int HealthLevel= 10;
+     int HungerLevel= 8;
+     int Stateofmind = 9;
+
+
+     public void favouritefood(){
+         System.out.println("fish");
+
+     }
+
+     public void favouriteactivity(){
+String name;
+         System.out.println("running");
+     }
+            }
+
+
+class Adopter {
+    String Name= "Rebeka";
+float money =23;
+
 }
+
+class Vet{
+    String Name="Ion";
+    String Speciality="bovines";
+}
+
+
